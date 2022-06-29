2e244d9e8b71116e07356d82b2647a8ac7dcf7b5
Oscar <54118125+oscar-byte15@users.noreply.github.com>
2022-06-28 23:30:09 -0500

    changing names

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/AdoptMeYaFrontend'),
+      dir: require('path').join(__dirname, './coverage/BCMetodoAleman'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
